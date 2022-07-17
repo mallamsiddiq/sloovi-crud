2fb6cf53c2125d0ab5af7258185713bc97d70e1d
akinyemi sodiq <mallamsiddiq@gmail.com>
2022-07-17 20:44:21 +0100

    My first commit

--- sloovi_crud/settings.py
+++ sloovi_crud/settings.py
@@ -15,7 +15,7 @@ from decouple import config, Csv
 
 
 import dj_database_url
-# import django_heroku
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -175,4 +175,4 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
