c63af4601b57b424336a4c065d4f439148ae9300
akinyemi sodiq <mallamsiddiq@gmail.com>
2022-07-19 10:12:10 +0100

    heroku mongo config

--- sloovi_crud/settings.py
+++ sloovi_crud/settings.py
@@ -4,9 +4,7 @@ from pathlib import Path
 from decouple import config, Csv
 
 
-# import dj_database_url
 import django_heroku
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
